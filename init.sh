4df141560a4ec25b9f9c283a6c97a46dc558ef04
nouchka <github@katagena.com>
2016-03-12 01:14:40 +0100

    Chown www-data

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo $SYMFONY_ENV
-cd $SYMFONY_DIRECTORY
 
 if [ ! -f "composer.json" ]; then
 	cd /tmp/
 	symfony new $SYMFONY_NAME
 	rsync -rz /tmp/$SYMFONY_NAME/ $SYMFONY_DIRECTORY/
-	cd $SYMFONY_DIRECTORY
 fi
+
+cd $SYMFONY_DIRECTORY
 
 if [ "$SYMFONY_ENV" == "dev" ]; then
 	composer install
@@ -20,3 +20,4 @@ else
 	php app/console cache:clear --env=$SYMFONY_ENV --no-debug
 	php app/console assetic:dump --env=$SYMFONY_ENV --no-debug
 fi
+chown -R www-data: $SYMFONY_DIRECTORY
